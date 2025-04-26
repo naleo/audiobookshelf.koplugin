961aac342f59b316b4d828d4a116f65763e2864d
Naleo Hyde <naleo@naleo.me>
2025-04-25 18:06:54 -1000

    Add Search Panel

--- audiobookshelf/audiobookshelfapi.lua
+++ audiobookshelf/audiobookshelfapi.lua
@@ -7,6 +7,7 @@ local socketutil = require("socketutil")
 local socket = require("socket")
 local logger = require("logger")
 local RenderImage = require("ui/renderimage")
+local util = require("util")
 
 local VERSION = require("audiobookshelf_version")
 
@@ -120,6 +121,32 @@ function AudiobookshelfApi:getLibraryItemCover(id)
         return result
     end
     logger.warn("AudiobookshelfApi: cannot get library items for library", id ,status or code)
+    logger.warn("AudiobookshelfApi: error:", response)
+
+end
+
+function AudiobookshelfApi:getSearchResults(id, search_query)
+    local sink = {}
+    local url_encoded_search_string = util.urlEncode(search_query)
+    -- this is "ebooks" base64 encoded, and the URL encoded, to only return library items with ebooks
+    local filters = "ebooks." .. "ZWJvb2s%3D"
+    local request = {
+        url = config.server .. "/api/libraries/" .. id .. "/search?q=" .. url_encoded_search_string .. "&filter=" .. filters,
+        method = "GET",
+        headers = {
+            ["Authorization"] = "Bearer " .. config.token,
+            ["User-Agent"] = T("audiobookshelf.koplugin/%1", table.concat(VERSION, ".")),
+        },
+        sink = ltn12.sink.table(sink),
+    }
+    socketutil:set_timeout()
+    local code, _, status = socket.skip(1, http.request(request))
+    local response = table.concat(sink)
+    if code == 200 and response ~= "" then
+        local _, result = pcall(JSON.decode, response, JSON.decode.simple)
+        return result
+    end
+    logger.warn("AudiobookshelfApi: cannot search library", id ,search_query, status or code)
     logger.warn("AudiobookshelfApi: error:", response)
 
 end
--- audiobookshelf/audiobookshelfbrowser.lua
+++ audiobookshelf/audiobookshelfbrowser.lua
@@ -1,20 +1,26 @@
 local AudiobookshelfApi = require("audiobookshelf/audiobookshelfapi")
 local BookDetailsWidget = require("audiobookshelf/bookdetailswidget")
+local InputDialog = require("ui/widget/inputdialog")
 local Menu = require("ui/widget/menu")
 local UIManager = require("ui/uimanager")
 local _ = require("gettext")
 local logger = require("logger")
 
 local AudiobookshelfBrowser = Menu:extend{
-    no_title= false,
+    no_title = false,
     title = _("Audiobookshelf Browser"),
     is_popout = false,
     is_borderless = true,
+    title_bar_left_icon = "appbar.settings",
     show_parent = nil
 }
 
+-- levels:
+-- abs
+-- library
 function AudiobookshelfBrowser:init()
     self.show_parent = self
+    self.current_level = "abs"
     if self.item then
     else
         self.item_table = self:genItemTableFromLibraries()
@@ -35,10 +41,6 @@ function AudiobookshelfBrowser:genItemTableFromLibraries()
     return item_table
 end
 
-function AudiobookshelfBrowser:getItemTableFromLibrary()
-    logger.log("hi")
-end
-
 function AudiobookshelfBrowser:onMenuSelect(item)
     if item.type == "library" then
         table.insert(self.paths, {
@@ -54,6 +56,72 @@ function AudiobookshelfBrowser:onMenuSelect(item)
         return true
 end
 
+function AudiobookshelfBrowser:onLeftButtonTap()
+    if self.level == "abs" then
+        -- this is the settings gear button
+        logger.info("settings")
+    elseif self.level == "library" then
+        self:ShowSearch()
+    end
+end
+
+function AudiobookshelfBrowser:ShowSearch()
+    self.search_dialog = InputDialog:new{
+        title = "Search",
+        input = self.search_value,
+        buttons = {
+            {
+                {
+                    text = "Cancel",
+                    id = "close",
+                    enabled = true,
+                    callback = function()
+                        self.search_dialog:onClose()
+                        UIManager:close(self.search_dialog)
+                    end
+                },
+                {
+                    text = "Search",
+                    enabled = true,
+                    callback = function()
+                        self.search_value = self.search_dialog:getInputText()
+                        self:search()
+                    end
+                }
+            }
+        }
+    }
+    UIManager:show(self.search_dialog)
+    self.search_dialog:onShowKeyboard()
+end
+
+function AudiobookshelfBrowser:search()
+    if self.search_value then
+        self.search_dialog:onClose()
+        UIManager:close(self.search_dialog)
+        if string.len(self.search_value) > 0 then
+            self:loadLibrarySearch(self.search_value)
+        end
+    end
+end
+
+function AudiobookshelfBrowser:loadLibrarySearch(search)
+    local tbl = {}
+    local libraryItems = AudiobookshelfApi:getSearchResults(self.library_id, search)
+    logger.warn(libraryItems)
+    for _, item in ipairs(libraryItems.book) do
+        table.insert(tbl, {
+            id = item.libraryItem.id,
+            text = item.libraryItem.media.metadata.title,
+            mandatory = item.libraryItem.media.metadata.authorName,
+            type = "book"
+        })
+    end
+
+    self:setTitleBarLeftIcon("appbar.search")
+    self:switchItemTable("Search Results", tbl)
+end
+
 function AudiobookshelfBrowser:openLibrary(id, name)
     local tbl = {}
     local libraryItems = AudiobookshelfApi:getLibraryItems(id)
@@ -62,13 +130,18 @@ function AudiobookshelfBrowser:openLibrary(id, name)
             id = item.id,
             text = item.media.metadata.title,
             mandatory = item.media.metadata.authorName,
-            url = "test",
             type = "book"
         })
     end
 
+    self.library_id = id
+    self.level = "library"
+    self:setTitleBarLeftIcon("appbar.search")
     self:switchItemTable(name, tbl)
     return true
 end
+
+
+
 
 return AudiobookshelfBrowser
